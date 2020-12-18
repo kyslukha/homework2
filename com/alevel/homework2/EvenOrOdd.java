d3b044f94d3bf9075661b0105c823e9b1f4e4f4f
kyslukha <kyslukha@gmail.com>
2020-12-18 21:50:02 +0200

    change codestyle

--- com/alevel/homework2/EvenOrOdd.java
+++ com/alevel/homework2/EvenOrOdd.java
@@ -2,7 +2,7 @@ package com.alevel.homework2;
 
 public class EvenOrOdd {
     public static void main(String[] args) {
-        int number = 6;
+        int number = 7;
         System.out.println(number);
         String answer = (number % 2 == 0) ? "even number" : "odd number";
         System.out.println(answer);
--- com/alevel/homework2/Least.java
+++ com/alevel/homework2/Least.java
@@ -2,19 +2,19 @@ package com.alevel.homework2;
 
 public class Least {
     public static void main(String[] args) {
-        int number1 = -7;
-        int number2 = - 100;
-        int number3 = 3;
+        int number1 = -77;
+        int number2 = - 10;
+        int number3 = 33;
         System.out.println(number1);
         System.out.println(number2);
         System.out.println(number3); // I just want to add more iteration
         if ((Math.abs(number1) < Math.abs(number2)) && (Math.abs(number1) < Math.abs(number3))) {
-            System.out.println("least number is " + number1);
+            System.out.println("Least number is " + number1);
         } else {
-            if (number2 < number3) {
-                System.out.println("least number is " + number2);
+            if (Math.abs(number2) < Math.abs(number3)) {
+                System.out.println("Least number is " + number2);
             } else {
-                System.out.println("least number is " + number3);
+                System.out.println("Least number is " + number3);
             }
         }
     }
