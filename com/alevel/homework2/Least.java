c01b5653f163d3876afdd5b72e5f9dcb3ff6630b
kyslukha <kyslukha@gmail.com>
2020-12-28 11:13:36 +0200

    fourth commit

--- com/alevel/homework2/Least.java
+++ com/alevel/homework2/Least.java
@@ -2,16 +2,16 @@ package com.alevel.homework2;
 
 public class Least {
     public static void main(String[] args) {
-        int number1 = -77;
-        int number2 = - 10;
-        int number3 = 33;
+        int number1 = -7;
+        int number2 = 100;
+        int number3 = -300;
         System.out.println(number1);
         System.out.println(number2);
         System.out.println(number3); // I just want to add more iteration
         if ((Math.abs(number1) < Math.abs(number2)) && (Math.abs(number1) < Math.abs(number3))) {
             System.out.println("Least number is " + number1);
         } else {
-            if (Math.abs(number2) < Math.abs(number3)) {
+            if (Math.abs(number2) < Math.abs(number3)) {//find a mistake
                 System.out.println("Least number is " + number2);
             } else {
                 System.out.println("Least number is " + number3);
